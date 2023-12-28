5815d0984a77af19903bec36399922025b8657b1
payzinn <crazyff23124@gmail.com>
2023-12-28 19:29:58 +0600

    qwr

--- recursion.py
+++ recursion.py
@@ -12,7 +12,10 @@ def second():
     def fibonacci(num):
         if num == 1:
             return 0
-        elif num == 2:
+        if num == 2:
             return 1
 
         return fibonacci(num - 1) + fibonacci(num - 2)
+    
+    print(fibonacci(10))
+second()
