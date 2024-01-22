079af0bf61a8e23d5aae50fd49cf4c226bd02d4e
payzinn <crazyff23124@gmail.com>
2024-01-22 12:44:17 +0600

    решил ргб

--- tkinter/programm.py
+++ tkinter/programm.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+from tkinter import messagebox
 root = Tk()
 root.title('MyWindow')
 root.geometry('600x400')
@@ -9,12 +9,15 @@ def changecolor(*args):
     for var in (str_var1, str_var2, str_var3):
         value = var.get().strip()
         if value:
-            color.append(int(value))
+            if value.isdigit() and int(value) >= 0 and int(value) <= 255:
+                color.append(int(value))
+            else:
+                messagebox.showinfo('Ошибка', 'Значение должно быть числом от 0 до 255')
         else:
             color.append(0)
-    root.configure(bg=_from_rgb(tuple(color)))
+    root.configure(bg= from_rgb(tuple(color)))
             
-def _from_rgb(rgb):
+def from_rgb(rgb):
     """translates an rgb tuple of int to a tkinter friendly color code
     """
     return "#%02x%02x%02x" % rgb
